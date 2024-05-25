48c2905955d66dd6828a4c4bcb5c371ef87d00f8
aben20807 <aben20807@gmail.com>
2024-05-25 11:16:35 +0400

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# FetcH
+
+```bash
+$ python3 fetch.py --help
+usage: FetcH [-h] [--dir DIR] url
+
+Fetch HackMD bookmode files
+
+positional arguments:
+  url         Root URL of HackMD bookmode
+
+options:
+  -h, --help  show this help message and exit
+  --dir DIR   Output directory
+
+Author: Po-Hsuan Huang (aben20807)
+```
---
+++ fetch.py
@@ -0,0 +1,76 @@
+import argparse
+import requests
+import re
+import os
+
+from urllib.parse import unquote
+
+
+file_counter = 0
+
+
+def get_filename(response, width):
+    global file_counter
+    filename = f"{file_counter:0{width}}"
+    file_counter += 1
+    find_filename = re.search(
+        r'filename="(.*)"', response.headers["Content-Disposition"]
+    )
+    if find_filename:
+        filename += "-" + unquote(find_filename.group(1)).replace(" ", "-")
+    else:
+        print("No filename found. Using default name.")
+        filename += ".md"
+    return filename
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog="FetcH",
+        description="Fetch HackMD bookmode files",
+        epilog="Author: Po-Hsuan Huang (aben20807)",
+    )
+    parser.add_argument("url", type=str, help="Root URL of HackMD bookmode")
+    parser.add_argument("--dir", type=str, default="docs/", help="Output directory")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    root_url = args.url
+    root_dir = args.dir
+
+    # Fetch root
+    root_response = requests.get(root_url + "/download")
+
+    # Get hackmd urls
+    hackmd_urls = []
+    root_content = root_response.text
+    for line in root_content.split("\n"):
+        find_hackmd_url = re.search(r"(https://hackmd.io/.*)\)", line)
+        if find_hackmd_url:
+            hackmd_urls.append(find_hackmd_url.group(1))
+
+    # Make the width of the prefix index
+    width = len(str(len(hackmd_urls) + 1))
+    root_filename = get_filename(root_response, width)
+    print(root_filename)
+
+    # Write root file
+    os.makedirs(root_dir, exist_ok=True)
+    with open(root_dir + root_filename, "w", encoding="utf-8") as root_file:
+        root_file.write(root_content)
+
+    # Fetch and write files
+    for hackmd_url in hackmd_urls:
+        hackmd_response = requests.get(hackmd_url + "/download")
+        hackmd_content = hackmd_response.text
+        hackmd_filename = get_filename(hackmd_response, width)
+        print(hackmd_filename)
+        with open(root_dir + hackmd_filename, "w", encoding="utf-8") as hackmd_file:
+            hackmd_file.write(hackmd_content)
+
+
+if __name__ == "__main__":
+    main()
